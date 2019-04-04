932f0b716ac3843f80ddb323f45afeb8e392354a
KarthikeyanKumar <karthikeyan.cse.skcet@gmail.com>
2019-04-04 16:55:55 +0530

    Support for GetConversionDetails and GetNetFundingInformation

---
+++ Samples/Reporting/CoreServices/GetNetFundingInformation.php
@@ -0,0 +1,31 @@
+	<?php
+require_once __DIR__. DIRECTORY_SEPARATOR .'../../../vendor/autoload.php';
+require_once __DIR__. DIRECTORY_SEPARATOR .'../../../Resources/ExternalConfiguration.php';
+
+function GetNetFundingInformation()
+{
+	$commonElement = new CyberSource\ExternalConfiguration();
+	$config = $commonElement->ConnectionHost();
+	$apiclient = new CyberSource\ApiClient($config);
+	$startTime="2018-10-01T00:00:00.0Z";
+	$endTime="2018-10-30T23:59:59.0Z";
+	$groupName="groupName";
+	$api_instance = new CyberSource\Api\NetFundingsApi($apiclient);
+	$orgId = "testrest";
+	$api_response = list($response,$statusCode,$httpHeader)=null;
+	try {
+		$api_response = $api_instance->getNetFundingInfo($startTime,$endTime,$orgId,$groupName);
+		echo "<pre>";print_r($api_response);
+
+	} catch (Cybersource\ApiException $e) {
+		print_r($e->getResponseBody());
+		print_r($e->getMessage());
+    }
+}    
+
+// Call Sample Code
+if(!defined('DO_NOT_RUN_SAMPLES')){
+    echo "Get Net Funding Information Samplecode is Running.. \n";
+	GetNetFundingInformation();
+}
+?>	
