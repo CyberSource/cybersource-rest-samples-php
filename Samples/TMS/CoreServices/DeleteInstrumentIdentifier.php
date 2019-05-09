c6aeb66f864d13fb3eda9a17db84546dd70c3e3f
snavinch <snavinch@visa.com>
2019-05-09 16:12:36 +0530

    - Removed a comment

--- Samples/TMS/CoreServices/DeleteInstrumentIdentifier.php
+++ Samples/TMS/CoreServices/DeleteInstrumentIdentifier.php
@@ -12,7 +12,6 @@ function DeleteInstrumentIdentifier()
   	$profileId = '93B32398-AD51-4CC2-A682-EA3E93614EB1';
 	require_once __DIR__. DIRECTORY_SEPARATOR .'RetrieveInstrumentIdentifier.php';
 		$tokenId = RetrieveInstrumentIdentifier(true);
-		print_r($tokenId);
 	$api_response = list($response,$statusCode,$httpHeader)=null;
 	try {
 		$api_response = $api_instance->deleteInstrumentIdentifier($profileId, $tokenId);
