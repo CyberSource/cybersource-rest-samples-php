d35de808e4ea83633f2eb9963bb403f6368dcdbd
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2021-04-01 19:05:12 +0530

    Upgrade to latest version

---
+++ Samples/Payments/Payments/AuthorizationWithTMSTokenBypassingNetworkToken.php
@@ -0,0 +1,70 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../../../vendor/autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../../../Resources/ExternalConfiguration.php';
+
+function AuthorizationWithTMSTokenBypassingNetworkToken()
+{
+	$clientReferenceInformationArr = [
+			"code" => "TC50171_3"
+	];
+	$clientReferenceInformation = new CyberSource\Model\Ptsv2paymentsClientReferenceInformation($clientReferenceInformationArr);
+
+	$paymentInformationInstrumentIdentifierArr = [
+			"id" => "7010000000016241111"
+	];
+	$paymentInformationInstrumentIdentifier = new CyberSource\Model\Ptsv2paymentsPaymentInformationInstrumentIdentifier($paymentInformationInstrumentIdentifierArr);
+
+	$paymentInformationArr = [
+			"instrumentIdentifier" => $paymentInformationInstrumentIdentifier
+	];
+	$paymentInformation = new CyberSource\Model\Ptsv2paymentsPaymentInformation($paymentInformationArr);
+
+	$orderInformationAmountDetailsArr = [
+			"totalAmount" => "102.21",
+			"currency" => "USD"
+	];
+	$orderInformationAmountDetails = new CyberSource\Model\Ptsv2paymentsOrderInformationAmountDetails($orderInformationAmountDetailsArr);
+
+	$orderInformationArr = [
+			"amountDetails" => $orderInformationAmountDetails
+	];
+	$orderInformation = new CyberSource\Model\Ptsv2paymentsOrderInformation($orderInformationArr);
+
+	$tokenInformationArr = [
+			"networkTokenOption" => "ignore"
+	];
+	$tokenInformation = new CyberSource\Model\Ptsv2paymentsTokenInformation($tokenInformationArr);
+
+	$requestObjArr = [
+			"clientReferenceInformation" => $clientReferenceInformation,
+			"paymentInformation" => $paymentInformation,
+			"orderInformation" => $orderInformation,
+			"tokenInformation" => $tokenInformation
+	];
+	$requestObj = new CyberSource\Model\CreatePaymentRequest($requestObjArr);
+
+
+	$commonElement = new CyberSource\ExternalConfiguration();
+	$config = $commonElement->ConnectionHost();
+	$merchantConfig = $commonElement->merchantConfigObject();
+
+	$api_client = new CyberSource\ApiClient($config, $merchantConfig);
+	$api_instance = new CyberSource\Api\PaymentsApi($api_client);
+
+	try {
+		$apiResponse = $api_instance->createPayment($requestObj);
+		print_r(PHP_EOL);
+		print_r($apiResponse);
+
+		return $apiResponse;
+	} catch (Cybersource\ApiException $e) {
+		print_r($e->getResponseBody());
+		print_r($e->getMessage());
+	}
+}
+
+if(!defined('DO_NOT_RUN_SAMPLES')){
+	echo "\nAuthorizationWithTMSTokenBypassingNetworkToken Sample Code is Running..." . PHP_EOL;
+	AuthorizationWithTMSTokenBypassingNetworkToken();
+}
+?>
