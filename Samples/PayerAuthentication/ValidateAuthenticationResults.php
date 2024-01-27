7da1559081698b5e42f1f98201ead0cbe50cbdd5
monu-kumar-visa <138751796+monu-kumar-visa@users.noreply.github.com>
2024-01-27 23:46:18 +0530

    api-spec changes

--- Samples/PayerAuthentication/ValidateAuthenticationResults.php
+++ Samples/PayerAuthentication/ValidateAuthenticationResults.php
@@ -5,61 +5,51 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '../../Resources/ExternalConfigurat
 function ValidateAuthenticationResults()
 {
     $clientReferenceInformationPartnerArr = [
-            "developerId" => "7891234",
-            "solutionId" => "89012345"
+        "developerId" => "7891234",
+        "solutionId" => "89012345"
     ];
     $clientReferenceInformationPartner = new CyberSource\Model\Riskv1decisionsClientReferenceInformationPartner($clientReferenceInformationPartnerArr);
 
     $clientReferenceInformationArr = [
-            "code" => "pavalidatecheck",
-            "partner" => $clientReferenceInformationPartner
+        "code" => "pavalidatecheck",
+        "partner" => $clientReferenceInformationPartner
     ];
     $clientReferenceInformation = new CyberSource\Model\Riskv1decisionsClientReferenceInformation($clientReferenceInformationArr);
 
     $orderInformationAmountDetailsArr = [
-            "currency" => "USD",
-            "totalAmount" => "200.00"
+        "currency" => "USD",
+        "totalAmount" => "200.00"
     ];
-    $orderInformationAmountDetails = new CyberSource\Model\Riskv1authenticationsOrderInformationAmountDetails($orderInformationAmountDetailsArr);
-
-    $orderInformationLineItems = array();
-    $orderInformationLineItems_0 = [
-            "unitPrice" => "10",
-            "quantity" => 2,
-            "taxAmount" => "32.40"
-    ];
-    $orderInformationLineItems[0] = new CyberSource\Model\Riskv1authenticationresultsOrderInformationLineItems($orderInformationLineItems_0);
+    $orderInformationAmountDetails = new CyberSource\Model\Riskv1authenticationresultsOrderInformationAmountDetails($orderInformationAmountDetailsArr);
 
     $orderInformationArr = [
-            "amountDetails" => $orderInformationAmountDetails,
-            "lineItems" => $orderInformationLineItems
+        "amountDetails" => $orderInformationAmountDetails
     ];
     $orderInformation = new CyberSource\Model\Riskv1authenticationresultsOrderInformation($orderInformationArr);
 
     $paymentInformationCardArr = [
-            "type" => "002",
-            "expirationMonth" => "12",
-            "expirationYear" => "2025",
-            "number" => "5200000000000007"
+        "type" => "002",
+        "expirationMonth" => "12",
+        "expirationYear" => "2025",
+        "number" => "5200000000000007"
     ];
     $paymentInformationCard = new CyberSource\Model\Riskv1authenticationresultsPaymentInformationCard($paymentInformationCardArr);
 
     $paymentInformationArr = [
-            "card" => $paymentInformationCard
+        "card" => $paymentInformationCard
     ];
     $paymentInformation = new CyberSource\Model\Riskv1authenticationresultsPaymentInformation($paymentInformationArr);
 
     $consumerAuthenticationInformationArr = [
-            "authenticationTransactionId" => "PYffv9G3sa1e0CQr5fV0",
-            "signedPares" => "eNqdmFmT4jgSgN+J4D90zD4yMz45PEFVhHzgA2zwjXnzhQ984Nvw61dAV1"
+        "authenticationTransactionId" => "PYffv9G3sa1e0CQr5fV0"
     ];
     $consumerAuthenticationInformation = new CyberSource\Model\Riskv1authenticationresultsConsumerAuthenticationInformation($consumerAuthenticationInformationArr);
 
     $requestObjArr = [
-            "clientReferenceInformation" => $clientReferenceInformation,
-            "orderInformation" => $orderInformation,
-            "paymentInformation" => $paymentInformation,
-            "consumerAuthenticationInformation" => $consumerAuthenticationInformation
+        "clientReferenceInformation" => $clientReferenceInformation,
+        "orderInformation" => $orderInformation,
+        "paymentInformation" => $paymentInformation,
+        "consumerAuthenticationInformation" => $consumerAuthenticationInformation
     ];
     $requestObj = new CyberSource\Model\ValidateRequest($requestObjArr);
 
@@ -75,8 +65,8 @@ function ValidateAuthenticationResults()
         $apiResponse = $api_instance->validateAuthenticationResults($requestObj);
         print_r(PHP_EOL);
         print_r($apiResponse);
-
         WriteLogAudit($apiResponse[1]);
+
         return $apiResponse;
     } catch (Cybersource\ApiException $e) {
         print_r($e->getResponseBody());
@@ -93,7 +83,7 @@ if (!function_exists('WriteLogAudit')){
     }
 }
 
-if(!defined('DO_NOT_RUN_SAMPLES')){
+if(!defined('DO_NOT_RUN_SAMPLES')) {
     echo "\nValidateAuthenticationResults Sample Code is Running..." . PHP_EOL;
     ValidateAuthenticationResults();
 }
