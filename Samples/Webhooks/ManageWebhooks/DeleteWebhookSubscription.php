6f32c5633ecf46d306bb28b4b70720499de88368
gaubansa <gaubansa@visa.com>
2025-03-28 00:04:52 +0530

    commenting webhook samples

--- Samples/Webhooks/ManageWebhooks/DeleteWebhookSubscription.php
+++ Samples/Webhooks/ManageWebhooks/DeleteWebhookSubscription.php
@@ -5,26 +5,26 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '../../../Resources/ExternalConfigu
 function DeleteWebhookSubscription($webhookId)
 {
 
-    $commonElement = new CyberSource\ExternalConfiguration();
-    $config = $commonElement->ConnectionHost();
-    $merchantConfig = $commonElement->merchantConfigObject();
+    // $commonElement = new CyberSource\ExternalConfiguration();
+    // $config = $commonElement->ConnectionHost();
+    // $merchantConfig = $commonElement->merchantConfigObject();
 
-    $api_client = new CyberSource\ApiClient($config, $merchantConfig);
-    $api_instance = new CyberSource\Api\ManageWebhooksApi($api_client);
+    // $api_client = new CyberSource\ApiClient($config, $merchantConfig);
+    // $api_instance = new CyberSource\Api\ManageWebhooksApi($api_client);
 
-    try {
-        $apiResponse = $api_instance->deleteWebhookSubscription($webhookId);
-        print_r(PHP_EOL);
-        print_r($apiResponse);
-        WriteLogAudit($apiResponse[1]);
+    // try {
+    //     $apiResponse = $api_instance->deleteWebhookSubscription($webhookId);
+    //     print_r(PHP_EOL);
+    //     print_r($apiResponse);
+    //     WriteLogAudit($apiResponse[1]);
 
-        return $apiResponse;
-    } catch (Cybersource\ApiException $e) {
-        print_r($e->getResponseBody());
-        print_r($e->getMessage());
-        $errorCode = $e->getCode();
-        WriteLogAudit($errorCode);
-    }
+    //     return $apiResponse;
+    // } catch (Cybersource\ApiException $e) {
+    //     print_r($e->getResponseBody());
+    //     print_r($e->getMessage());
+    //     $errorCode = $e->getCode();
+    //     WriteLogAudit($errorCode);
+    // }
 }
 
 if (!function_exists('WriteLogAudit')){
--- Samples/Webhooks/ManageWebhooks/ReplayFailedTransactionsInLast24Hours.php
+++ Samples/Webhooks/ManageWebhooks/ReplayFailedTransactionsInLast24Hours.php
@@ -4,40 +4,40 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '../../../Resources/ExternalConfigu
 
 function ReplayFailedTransactionsInLast24Hours($webhookId)
 {
-    $byDeliveryStatusArr = [
-            "status" => "FAILED",
-            "hoursBack" => 24,
-            "productId" => "tokenManagement",
-            "eventType" => "tms.token.created"
-    ];
-    $byDeliveryStatus = new CyberSource\Model\Nrtfv1webhookswebhookIdreplaysByDeliveryStatus($byDeliveryStatusArr);
+    // $byDeliveryStatusArr = [
+    //         "status" => "FAILED",
+    //         "hoursBack" => 24,
+    //         "productId" => "tokenManagement",
+    //         "eventType" => "tms.token.created"
+    // ];
+    // $byDeliveryStatus = new CyberSource\Model\Nrtfv1webhookswebhookIdreplaysByDeliveryStatus($byDeliveryStatusArr);
 
-    $requestObjArr = [
-            "byDeliveryStatus" => $byDeliveryStatus
-    ];
-    $requestObj = new CyberSource\Model\ReplayWebhooksRequest($requestObjArr);
+    // $requestObjArr = [
+    //         "byDeliveryStatus" => $byDeliveryStatus
+    // ];
+    // $requestObj = new CyberSource\Model\ReplayWebhooksRequest($requestObjArr);
 
 
-    $commonElement = new CyberSource\ExternalConfiguration();
-    $config = $commonElement->ConnectionHost();
-    $merchantConfig = $commonElement->merchantConfigObject();
+    // $commonElement = new CyberSource\ExternalConfiguration();
+    // $config = $commonElement->ConnectionHost();
+    // $merchantConfig = $commonElement->merchantConfigObject();
 
-    $api_client = new CyberSource\ApiClient($config, $merchantConfig);
-    $api_instance = new CyberSource\Api\ManageWebhooksApi($api_client);
+    // $api_client = new CyberSource\ApiClient($config, $merchantConfig);
+    // $api_instance = new CyberSource\Api\ManageWebhooksApi($api_client);
 
-    try {
-        $apiResponse = $api_instance->replayPreviousWebhooks($webhookId, $requestObj);
-        print_r(PHP_EOL);
-        print_r($apiResponse);
-        WriteLogAudit($apiResponse[1]);
+    // try {
+    //     $apiResponse = $api_instance->replayPreviousWebhooks($webhookId, $requestObj);
+    //     print_r(PHP_EOL);
+    //     print_r($apiResponse);
+    //     WriteLogAudit($apiResponse[1]);
 
-        return $apiResponse;
-    } catch (Cybersource\ApiException $e) {
-        print_r($e->getResponseBody());
-        print_r($e->getMessage());
-        $errorCode = $e->getCode();
-        WriteLogAudit($errorCode);
-    }
+    //     return $apiResponse;
+    // } catch (Cybersource\ApiException $e) {
+    //     print_r($e->getResponseBody());
+    //     print_r($e->getMessage());
+    //     $errorCode = $e->getCode();
+    //     WriteLogAudit($errorCode);
+    // }
 }
 
 if (!function_exists('WriteLogAudit')){
